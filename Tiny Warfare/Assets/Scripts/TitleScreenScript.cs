601173eb19f8a15dadc9681024e96a97b9175411
RossH6348 <HUT18001284@Students.solihull.ac.uk>
2023-03-18 01:13:01 +0000

    Lobby related UI dialogs, preparation for lobby networking

--- Tiny Warfare/Assets/Scripts/TitleScreenScript.cs
+++ Tiny Warfare/Assets/Scripts/TitleScreenScript.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
 
 public class TitleScreenScript : MonoBehaviour
 {
+
+
+    [SerializeField] private NetworkManager Multiplayer;
+
     [SerializeField] private Camera titleCamera;
 
     [SerializeField] private Transform cameraTarget;
     [SerializeField] private List<Transform> cameraLocations = new List<Transform>();
 
+    [SerializeField] private GameObject networkDialog;
+    [SerializeField] private Text networkTopic;
+    [SerializeField] private Text networkText;
+
+    [SerializeField] private GameObject joinDialog;
+    [SerializeField] private InputField joinInput;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        networkDialog.SetActive(false);
+        joinDialog.SetActive(false);
+        networkTopic.text = networkText.text = joinInput.text = "";
 
     }
 
@@ -39,6 +56,43 @@ public class TitleScreenScript : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    //All networking related stuff will be done here.
+    private void networkMessage(string topic = "", string reason = "")
+    {
+        networkDialog.SetActive(true);
+        networkTopic.text = topic;
+        networkText.text = reason;
+    }
+
+    public void HostLobby()
+    {
+
+        Multiplayer.StartHost();
+
+        if (cameraTarget != cameraLocations[2])
+            cameraTarget = cameraLocations[2];
+
+    }
+
+    public void JoinLobby()
+    {
+
+        joinDialog.SetActive(true);
+        joinInput.text = "";
+
+    }
+
+    public void ConnectLobby()
+    {
+
+        Multiplayer.StartClient();
+        joinDialog.SetActive(false);
+
+        if (cameraTarget != cameraLocations[2])
+            cameraTarget = cameraLocations[2];
+
     }
 
 }
