25fdab0fa566190656588306beeece88134f9a59
RossH6348 <HUT18001284@Students.solihull.ac.uk>
2023-03-22 17:17:56 +0000

    Lobby Disconnect and reorganization

---
+++ Tiny Warfare/Assets/Scripts/ClientToNameScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ClientToNameScript
+{
+
+    public static Dictionary<ulong, string> playerNames = new Dictionary<ulong, string>();
+
+}
--- Tiny Warfare/Assets/Scripts/TitleScreenScript.cs
+++ Tiny Warfare/Assets/Scripts/TitleScreenScript.cs
@@ -65,13 +65,6 @@ public class TitleScreenScript : NetworkBehaviour
     }
 
     //All networking related stuff will be done here.
-    private void networkMessage(string topic = "", string reason = "")
-    {
-        networkDialog.SetActive(true);
-        networkTopic.text = topic;
-        networkText.text = reason;
-    }
-
     public void HostLobby()
     {
 
@@ -101,6 +94,13 @@ public class TitleScreenScript : NetworkBehaviour
 
     }
 
+    public void LeaveLobby()
+    {
+
+        LobbyDisconnectServerRpc();
+
+    }
+
     private void onLobbyJoin(ulong clientId)
     {
 
@@ -118,23 +118,65 @@ public class TitleScreenScript : NetworkBehaviour
 
     private void onLobbyLeave(ulong clientId)
     {
-
-        ClearLobbyClientRpc();
         NetworkManager.Shutdown();
-
+        ClearLobby();
+        MainScreen();
     }
 
-    List<string> LobbyPlayers = new List<string>();
-
     [ServerRpc(RequireOwnership = false)]
-    private void LobbyHandshakeServerRpc(string playerName)
+    private void LobbyHandshakeServerRpc(string playerName, ServerRpcParams serverRpcParams = default)
     {
 
         //Okay we received this client's name, so let add it to the server list and rebroadcast the current lobby status.
-        LobbyPlayers.Add(playerName);
+        ClientToNameScript.playerNames.Add(serverRpcParams.Receive.SenderClientId, playerName);
         ClearLobbyClientRpc();
-        foreach (string name in LobbyPlayers)
-            AddLobbyPlayerClientRpc(name);
+        foreach (KeyValuePair<ulong, string> clientData in ClientToNameScript.playerNames)
+            AddLobbyPlayerClientRpc(clientData.Value);
+
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void LobbyDisconnectServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+
+        //Okay this client wants to disconnect, let find the client's playername to remove and rebroadcast.
+        //Unless it is the host, then we will need to disconnect everyone with a reason.
+        ulong senderId = serverRpcParams.Receive.SenderClientId;
+        if (senderId == NetworkManager.ServerClientId)
+        {
+
+            //We will need to tell all other clients before disconnecting them that the host left.
+            foreach (ulong clientId in NetworkManager.ConnectedClientsIds)
+            {
+
+                if (clientId == senderId)
+                    continue; //Host does not need to see the reason.
+
+                ClientRpcParams clientRpcParams = new ClientRpcParams
+                {
+                    Send = new ClientRpcSendParams
+                    {
+                        TargetClientIds = new ulong[] { clientId }
+                    }
+                };
+
+                networkMessageClientRpc("Disconnected", "Host has left the lobby!", clientRpcParams);
+            }
+
+            NetworkManager.Shutdown();
+            ClearLobby();
+            MainScreen();
+            return;
+        }
+        else
+        {
+            ClientToNameScript.playerNames.Remove(serverRpcParams.Receive.SenderClientId);
+            NetworkManager.DisconnectClient(serverRpcParams.Receive.SenderClientId);
+        }
+
+        ClearLobbyClientRpc();
+        foreach (KeyValuePair<ulong, string> clientData in ClientToNameScript.playerNames)
+            AddLobbyPlayerClientRpc(clientData.Value);
 
     }
 
@@ -142,21 +184,36 @@ public class TitleScreenScript : NetworkBehaviour
     private void LobbyHandshakeClientRpc(ClientRpcParams clientRpcParams = default)
     {
 
+        //Response back to the handshake with our player name.
         LobbyHandshakeServerRpc(options.playerInputField.text);
 
     }
 
     [ClientRpc]
-    private void ClearLobbyClientRpc()
+    private void ClearLobbyClientRpc(ClientRpcParams clientRpcParams = default)
+    {
+
+        ClearLobby();
+
+    }
+
+    private void ClearLobby()
     {
         foreach (GameObject soldier in LobbySoldiers)
             soldier.SetActive(false);
     }
 
     [ClientRpc]
-    private void AddLobbyPlayerClientRpc(string playerName)
+    private void AddLobbyPlayerClientRpc(string playerName, ClientRpcParams clientRpcParams = default)
     {
-        foreach(GameObject soldier in LobbySoldiers)
+
+        AddLobbyPlayer(playerName);
+    }
+
+    private void AddLobbyPlayer(string playerName)
+    {
+
+        foreach (GameObject soldier in LobbySoldiers)
         {
             if (!soldier.activeSelf)
             {
@@ -166,6 +223,22 @@ public class TitleScreenScript : NetworkBehaviour
                     lobbyPlayer.initializePlayer(playerName, IsHost);
                 break;
             }
+        }
+    }
+
+    [ClientRpc]
+    private void networkMessageClientRpc(string topic = "", string reason = "", ClientRpcParams clientRpcParams = default)
+    {
+        networkDialog.SetActive(true);
+        networkTopic.text = topic;
+        networkText.text = reason;
+    }
+
+    void Cleanup()
+    {
+        if (NetworkManager.Singleton != null)
+        {
+            Destroy(NetworkManager.Singleton.gameObject);
         }
     }
 
